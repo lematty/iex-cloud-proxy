89bd05ef3bb1e7cf46fbcf0aca69948d45e953d1
Chris LeMatty <clematty@gmail.com>
2020-04-23 01:08:18 +0200

    added corporate actions

--- src/corporate-actions/corporate-actions.controller.ts
+++ src/corporate-actions/corporate-actions.controller.ts
@@ -1,4 +1,89 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { CorporateActionsService } from './corporate-actions.service';
+import { BonusIssue, Distribution, Dividend, ReturnOfCapital, RightsIssue, RightsToPurchase, SecurityReclassification, SecuritySwap, Spinoff, Split } from 'iex-cloud';
 
 @Controller('corporate-actions')
-export class CorporateActionsController {}
+export class CorporateActionsController {
+
+  constructor(private corporateActionsService: CorporateActionsService) {}
+
+  @Get('/bonus-issue')
+  async getBonusIssue(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly BonusIssue[]> {
+    return await this.corporateActionsService.getBonusIssue(symbol, refid);
+  }
+
+  @Get('/distribution')
+  async getDistribution(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly Distribution[]> {
+    return await this.corporateActionsService.getDistribution(symbol, refid);
+  }
+
+  @Get('/dividends')
+  async getDividends(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly Dividend[]> {
+    return await this.corporateActionsService.getDividends(symbol, refid);
+  }
+
+  @Get('/return-of-capital')
+  async getReturnOfCapital(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly ReturnOfCapital[]> {
+    return await this.corporateActionsService.getReturnOfCapital(symbol, refid);
+  }
+
+  @Get('/rights-issue')
+  async getRightsIssue(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly RightsIssue[]> {
+    return await this.corporateActionsService.getRightsIssue(symbol, refid);
+  }
+
+  @Get('/rights-to-purchase')
+  async getRightsToPurchase(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly RightsToPurchase[]> {
+    return await this.corporateActionsService.getRightsToPurchase(symbol, refid);
+  }
+
+  @Get('/security-reclassification')
+  async getSecurityReclassification(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly SecurityReclassification[]> {
+    return await this.corporateActionsService.getSecurityReclassification(symbol, refid);
+  }
+
+  @Get('/security-swap')
+  async getSecuritySwap(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly SecuritySwap[]> {
+    return await this.corporateActionsService.getSecuritySwap(symbol, refid);
+  }
+
+  @Get('/spinoff')
+  async getSpinoff(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly Spinoff[]> {
+    return await this.corporateActionsService.getSpinoff(symbol, refid);
+  }
+
+  @Get('/splits')
+  async getSplits(
+    @Query('symbol') symbol?: string,
+    @Query('refid') refid?: string,
+    ): Promise<readonly Split[]> {
+    return await this.corporateActionsService.getSplits(symbol, refid);
+  }
+}
--- src/corporate-actions/corporate-actions.service.ts
+++ src/corporate-actions/corporate-actions.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { BonusIssue, bonusIssue, Distribution, distribution, dividends, Dividend, ReturnOfCapital, returnOfCapital, RightsIssue, rightsIssue, RightsToPurchase, rightsToPurchase, SecurityReclassification, securityReclassification, SecuritySwap, securitySwap, Spinoff, spinoff, splits, Split } from 'iex-cloud';
 
 @Injectable()
-export class CorporateActionsService {}
+export class CorporateActionsService {
+
+  // https://iexcloud.io/docs/api/#bonus-issue
+  async getBonusIssue(symbol?: string, refid?: string): Promise<readonly BonusIssue[]> {
+    return await bonusIssue(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#distribution
+  async getDistribution(symbol?: string, refid?: string): Promise<readonly Distribution[]> {
+    return await distribution(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#dividends
+  async getDividends(symbol?: string, refid?: string): Promise<readonly Dividend[]> {
+    return await dividends(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#return-of-capital
+  async getReturnOfCapital(symbol?: string, refid?: string): Promise<readonly ReturnOfCapital[]> {
+    return await returnOfCapital(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#rights-issue
+  async getRightsIssue(symbol?: string, refid?: string): Promise<readonly RightsIssue[]> {
+    return await rightsIssue(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#right-to-purchase
+  async getRightsToPurchase(symbol?: string, refid?: string): Promise<readonly RightsToPurchase[]> {
+    return await rightsToPurchase(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#security-reclassification
+  async getSecurityReclassification(symbol?: string, refid?: string): Promise<readonly SecurityReclassification[]> {
+    return await securityReclassification(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#security-swap
+  async getSecuritySwap(symbol?: string, refid?: string): Promise<readonly SecuritySwap[]> {
+    return await securitySwap(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#spinoff
+  async getSpinoff(symbol?: string, refid?: string): Promise<readonly Spinoff[]> {
+    return await spinoff(symbol, refid);
+  }
+
+  // https://iexcloud.io/docs/api/#splits
+  async getSplits(symbol?: string, refid?: string): Promise<readonly Split[]> {
+    return await splits(symbol, refid);
+  }
+
+
+}
